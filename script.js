f747277eaa55cd7d920f555bec32af5c61283df8
cinar04 <a.cinartasal04@gmail.com>
2026-01-26 18:20:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ setTimeout(() => {
     document.querySelector(".top-bar").classList.remove("hidden");
     document.querySelector(".content").classList.remove("hidden");
     document.body.style.overflow = "auto";
-}, 1800);
+}, 1000);
